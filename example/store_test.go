846a2752fad0ea06d2eeff321d15bf4d88616a14
kirk91 <kirk91.han@gmail.com>
2019-09-10 11:16:00 +0800

    Refine example

--- example/store_test.go
+++ example/store_test.go
@@ -1,20 +1,28 @@
 package example
 
 import (
+	"context"
+
 	"github.com/kirk91/stats"
 	"github.com/kirk91/stats/sink/statsd"
 )
 
 const (
-	appid = "arch.samaritan"
-	ezone = "wg1"
+	appid  = "samaritan"
+	region = "aisa-ch"
+	zone   = "hz"
 )
 
 func ExampleStore() {
+	store := stats.NewStore(stats.NewStoreOption())
+
 	// NOTE: If the underlying sink supports tag feature, you could
 	// add it depends on demand. Otherwise, you should ignore the
 	// following section.
-	defaultTags := map[string]string{"ezone": ezone}
+	defaultTags := map[string]string{
+		"region": region,
+		"zone":   zone,
+	}
 	tagExtractStrategies := []stats.TagExtractStrategy{
 		{
 			// service.(<service_name>.)*
@@ -27,19 +35,32 @@ func ExampleStore() {
 			Regex: "^service(?:\\.).*?\\.redis\\.((.*?)\\.)",
 		},
 	}
-	store := stats.NewStore(stats.NewStoreOption())
-	store.SetTagOption(
-		stats.NewTagOption().
-			WithDefaultTags(defaultTags).
-			WithTagExtractStrategies(tagExtractStrategies...))
+	tagOption := stats.NewTagOption().WithDefaultTags(defaultTags).
+		WithTagExtractStrategies(tagExtractStrategies...)
+	store.SetTagOption(tagOption)
 
-	// statsd
+	// add statsd sink
 	statsdAddr := "127.0.0.1:8125"
 	statsdSink := statsd.NewSink(statsdAddr, appid)
 	store.AddSink(statsdSink)
 
+	// start the background routine which flushes the metrics periodically.
+	ctx, cancel := context.WithCancel(context.TODO())
+	loopDone := make(chan struct{})
+	go func() {
+		store.FlushingLoop(ctx)
+		close(loopDone)
+	}()
+	defer func() {
+		cancel()
+		<-loopDone
+	}()
+
+	// create a stats group, the metrics which under it will sharing the same prefix.
 	scope := store.CreateScope("listener")
-	scope.Counter("accept").Inc()
+	scope.Counter("conn_create").Inc()           // listener.conn_create
+	scope.Gauge("conn_active").Inc()             // listener.conn_active
+	scope.Histogram("conn_length_sec").Record(1) // listener.conn_length_sec
 
 	//Output:
 }
